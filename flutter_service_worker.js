614d660ff6891798304abeb05488eb9c05fb3b49
unknown <cymmer4@gmail.com>
2022-07-27 14:27:48 +0800

    Build made on 2022-07-27 14:27

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,9 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "a21b38227141c634d827cffa48ebb07e",
+  "assets/AssetManifest.json": "fb9a78136970ee7318a7dd84daadc42f",
 "assets/assets/login-background.png": "c3d698f473fa29fd092e0bbc52964a9d",
-"assets/build/web/assets/envConfig": "e8a2e13bdf03e4855f849be3c081a7cc",
+"assets/az-spot-app-web-build/assets/build/web/assets/envConfig": "e8a2e13bdf03e4855f849be3c081a7cc",
+"assets/az-spot-app-web-build/assets/envConfig": "e8a2e13bdf03e4855f849be3c081a7cc",
 "assets/envConfig": "e8a2e13bdf03e4855f849be3c081a7cc",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
@@ -16,13 +17,18 @@ const RESOURCES = {
 "assets/packages/toast/assets/toastify.css": "a85675050054f179444bc5ad70ffc635",
 "assets/packages/toast/assets/toastify.js": "e7006a0a033d834ef9414d48db3be6fc",
 "assets/packages/wakelock_web/assets/no_sleep.js": "7748a45cd593f33280669b29c2c8919a",
+"canvaskit/canvaskit.js": "c2b4e5f3d7a3d82aed024e7249a78487",
+"canvaskit/canvaskit.wasm": "4b83d89d9fecbea8ca46f2f760c5a9ba",
+"canvaskit/profiling/canvaskit.js": "ae2949af4efc61d28a4a80fffa1db900",
+"canvaskit/profiling/canvaskit.wasm": "95e736ab31147d1b2c7b25f11d4c32cd",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "6b915f0683976d2e6c294483e3dfbc7c",
-"/": "6b915f0683976d2e6c294483e3dfbc7c",
+"index.html": "0c224b58296659f3f4b46f5b3565c928",
+"/": "0c224b58296659f3f4b46f5b3565c928",
 "main.dart.js": "a75449764d736a6059a892441638c872",
 "manifest.json": "bfff27930d5bf7a0c059011b09890795",
 "version.json": "b6d3d6dd50e367bdf6b97553cc1daf28"
